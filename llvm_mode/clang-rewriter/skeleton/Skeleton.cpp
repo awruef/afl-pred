4b52126660700d2b396418f216b0e8199a9d1cee
Andrew Ruef <awruef@cs.umd.edu>
2018-10-02 21:51:34 +0000

    Ugh.

---
+++ llvm_mode/clang-rewriter/skeleton/Skeleton.cpp
@@ -0,0 +1,10 @@
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Sema/Sema.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace clang;
+
+
